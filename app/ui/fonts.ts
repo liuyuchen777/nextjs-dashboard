ecfbd2412dadc7154b95c0bf464bf47d3704e893
liuyuchen777 <liuyuchen.hust@gmail.com>
2024-12-26 22:19:39 +0900

    Remove unused font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,4 @@
-import { Inter, Irish_Grover, Lusitana } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const irishGrover = Irish_Grover({ weight: '400' });
-export const lusitana = Lusitana({ weight: '700' });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: '700' });
