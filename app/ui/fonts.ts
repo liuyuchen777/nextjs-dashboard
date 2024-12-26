ce0a141851471fd68509a95ae9152759b44d1013
liuyuchen777 <liuyuchen.hust@gmail.com>
2024-12-26 22:16:41 +0900

    Commit lesson 1-5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Irish_Grover, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const irishGrover = Irish_Grover({ weight: '400' });
+export const lusitana = Lusitana({ weight: '700' });
